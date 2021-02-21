5fbbd812c3a7915bce09d31285f13f0d832d35ff
kumar <kumar@host.docker.internal>
2021-02-21 16:18:04 +0530

    new v

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -8,6 +8,6 @@ public class HelloWorldController {
 
 	@GetMapping(path = "/")
 	public String helloWorld() {
-		return "{\"message\":\"Hello World Java v10-mine-GKE-deployment v10\"}";
+		return "{\"message\":\"Hello World Java v10-mine-GKE-deployment v101\"}";
 	}
 }
